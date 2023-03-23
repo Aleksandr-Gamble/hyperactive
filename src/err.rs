31f7805c2c55ad723b361d9c5e673fc4dcd8bd88
Aleksandr-Gamble <aleksandr.gamble@gmail.com>
2023-03-23 09:45:26 -0400

    added SimpleError

--- src/err.rs
+++ src/err.rs
@@ -1,7 +1,37 @@
 //! This module contains errors.
 //! 
 
+
+use std::{error::Error, fmt};
+use serde::{Serialize, Deserialize};
+
+
 /// The GenericError encompasses almost every possible error type that could be passed.
 /// Asynchronous functions that return Result<T, GenericError> can call other functions and use the "?" operator to return the Err() variant as needed.
  
 pub type GenericError = Box<dyn std::error::Error + Send + Sync>;
+
+
+/// The SimpleError is easy to instantiate when a more specific error is not needed
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SimpleError {
+    // A very generic error. This is a bit of an antipattern,
+    // but it is easier than creating a new error types for a hundred misc things
+    pub message: String,
+}
+
+impl Error for SimpleError {}
+
+impl fmt::Display for SimpleError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "SimpleError: {}", self.message)
+    }
+}
+
+impl SimpleError {
+    pub fn from_str(message: &str) -> Self {
+        SimpleError{
+            message: message.to_string()
+        }
+    }
+}
